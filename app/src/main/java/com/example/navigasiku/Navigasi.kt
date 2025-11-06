8372727825217988d5fb1e24e2e32bae6daa376b
mahasiswa229 <mafaqih1945@gmail.com>
2025-11-06 09:15:54 +0700

    tambah composable data app (scaffold)

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -1,8 +1,10 @@
 package com.example.navigasiku
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import java.lang.reflect.Modifier
 
@@ -16,5 +18,9 @@ fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ){
-
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+        )
+    }
 }
