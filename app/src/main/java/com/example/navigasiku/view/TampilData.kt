fcca5cda901994efc3f4895c73ef512fd84f689c
mahasiswa229 <mafaqih1945@gmail.com>
2025-11-06 13:33:23 +0700

    buat composable (scaffold)

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -10,4 +10,24 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
         Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
     )
+
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.tampil),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        )
+    }
 }
