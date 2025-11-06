688d5b57c051a08e32f77c3f75dd14f62858b51b
mahasiswa229 <mafaqih1945@gmail.com>
2025-11-06 13:22:30 +0700

    buat composable (isi ruang)

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -21,7 +21,35 @@ fun FormIsian(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {},
+            )
 
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(all = 20.dp)
+                    .width(width = 250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+
+            Row {
+                jenisK.forEach { item ->
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        RadioButton(
+                            selected = false,
+                            onClick = { item }
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
         }
     }
 }
