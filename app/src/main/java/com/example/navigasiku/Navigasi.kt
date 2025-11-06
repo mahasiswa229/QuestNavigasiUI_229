d956991020c5e0319e88853e280f0139958bdfba
mahasiswa229 <mafaqih1945@gmail.com>
2025-11-06 13:48:06 +0700

    perbaiki code

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -8,6 +8,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.navigasiku.view.FormIsian
+import com.example.navigasiku.view.TampilData
 
 enum class Navigasi {
     Formulir,
@@ -25,20 +27,19 @@ fun DataApp(
             startDestination = Navigasi.Formulir.name,
 
             modifier= Modifier.padding( paddingValues = isiRuang)
-        ){
-            composable (route = Navigasi.Formulir.name) {
+        ) {
+            composable(route = Navigasi.Formulir.name) {
                 FormIsian(
                     OnSubmitBtnClick = {
                         navController.navigate(route = Navigasi.Detail.name)
                     }
                 )
             }
-            composable (route = Navigasi.Detail.name) {
+            composable(route = Navigasi.Detail.name) {
                 TampilData(
-                    OnBackBtnClick = {cancelAndBackToFormulir(navController)}
+                    onBackBtnClick = { cancelAndBackToFormulir(navController) }
                 )
             }
-
         }
     }
 }
