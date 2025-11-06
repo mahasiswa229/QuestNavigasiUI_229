14b444e8b659d53ab2467d593624099395eadb4f
mahasiswa229 <mafaqih1945@gmail.com>
2025-11-06 13:18:24 +0700

    buat composable formisian

--- app/src/main/java/com/example/navigasiku/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -1,5 +1,11 @@
 @file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.navigasiku.view
 
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick: () -> Unit
+) {
 
+}
 
